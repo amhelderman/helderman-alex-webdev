e4f4a973d2f9af6d84ccaf36e384de034e6f8b90
amhelderman <alexhelderman@gmail.com>
2017-07-19 18:13:55 -0400

    move javascript to IFFEs

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,67 +3,91 @@
  */
 
 
-var app = angular.module("WamApp", ["ngRoute"]);
+/* Handle Angular Application */
+
+(function (){
+
+    var app = angular.module("WamApp", ["ngRoute"]);
+
+    /*=========================================================================================*/
+    /* Routing*/
+    app.config( function($routeProvider)
+                {
+                    $routeProvider
+                        .when("/:pageName",
+                            {
+                                /* template URL can be a function:
+                                 https://docs.angularjs.org/api/ngRoute/provider/$routeProvider
+                                 */
+                                templateUrl: function (params) {
+                                    return params.pageName + '.html';
+                                }
+                            })
+                });
+
+    /*=========================================================================================*/
+    /*      Controllers */
+
+    // Login Controller
+
+    app.controller("loginController",
+        function ($scope, $location){
+            $scope.hello = "hello from loginController";
 
 
-/* Routing*/
-app.config(Config);
-app.controller("loginController", loginController);
-app.controller("profileController", profileController);
-
-function Config($routeProvider)
-{
-    $routeProvider
-        .when("/:pageName",
-            {
-                /* template URL can be a function:
-                 https://docs.angularjs.org/api/ngRoute/provider/$routeProvider
-                 */
-                templateUrl: function (params) {
-                    return params.pageName + '.html';
+            // When logging in...
+            $scope.login = function(user){
+                var users=[
+                    {_id: "123", username: "alice", password: "alice"}
+                ];
+                for( var u in users){
+                    var currentUser = users[u];
+                    if(currentUser.username === $scope.user.username & currentUser.password === $scope.user.password)
+                    {
+                        // alert("changing to login");
+                        $location.url("profile/"+currentUser._id);
+                    }
                 }
-            })
-};
-
-
-function loginController($scope, $location){
-    $scope.hello = "hello from loginController";
-
-
-    // When logging in...
-    $scope.login = function(user){
-        var users=[
-            {_id: "123", username: "alice", password: "alice"}
-        ];
-        for( var u in users){
-            var currentUser = users[u];
-            if(currentUser.username === $scope.user.username & currentUser.password === $scope.user.password)
-            {
-                // alert("changing to login");
-                $location.url("#profile/"+currentUser.username);
+                $scope.errorMessage = "User not found";
+                $scope.userName = currentUser.username;
             }
-        }
-        $scope.errorMessage = "User not found";
-        $scope.userName = currentUser.username;
-    }
-}
+        });
 
-function profileController($scope, $location){
+    /*=========================================================================================*/
 
-    $scope.hello = "hello from profile";
+    // Profile Controller
 
-    var users=[
-        {_id: "123", username: "alice", password: "alice"},
-        {_id: "124", username: "jerry", password: "jerry"}
-    ];
+    app.controller("profileController", function ($scope, $location){
+            $scope.hello = "hello from profile";
+
+            var users=[
+                {_id: "123", username: "alice", password: "alice"},
+                {_id: "124", username: "jerry", password: "jerry"}
+            ];
+
+            for(u in users)
+            {
+                var currentUser = users[u];
+
+                /* TODO */
+                if(currentUser._id == $location)
+                {
+                    // alert("changing to login");
+                    $scope.user = currentUser;
+                }
+            }
+
+            $scope.userName =
+
+            // When logging in...
+            $scope.login = function(user){
+            }
 
 
-    // When logging in...
-    $scope.login = function(user){
-    }
+        });
 
+})();
 
-}
 
 
 
