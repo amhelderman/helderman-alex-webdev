03e297b590576e72ff039c24143b9e2c970055dd
amhelderman <alexhelderman@gmail.com>
2017-07-19 00:03:05 -0400

    Trying maps API

--- public/project/app.js
+++ public/project/app.js
@@ -79,6 +79,17 @@ function profileController($scope, $location){
 
 
 /* GEO LOCATION */
+// function initMap() {
+//     var uluru = {lat: -25.363, lng: 131.044};
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 4,
+//         center: uluru
+//     });
+//     var marker = new google.maps.Marker({
+//         position: uluru,
+//         map: map
+//     });
+// }
 function initMap() {
     var uluru = {lat: -25.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -90,3 +101,41 @@ function initMap() {
         map: map
     });
 }
+//
+//
+// var x = document.getElementById("demo");
+//
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition, showError);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+//
+// function showPosition(position) {
+//     var latlon = position.coords.latitude + "," + position.coords.longitude;
+//     //             http://maps.googleapis.com/maps/api/service/output?parameters
+//     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+//         +latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyApxo8W1f-xebWiMEjYMyphOI5eUHxu8pg";
+//     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+// }
+// //To use this code on your website, get a free API key from Google.
+// //Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+//
+// function showError(error) {
+//     switch(error.code) {
+//         case error.PERMISSION_DENIED:
+//             x.innerHTML = "User denied the request for Geolocation."
+//             break;
+//         case error.POSITION_UNAVAILABLE:
+//             x.innerHTML = "Location information is unavailable."
+//             break;
+//         case error.TIMEOUT:
+//             x.innerHTML = "The request to get user location timed out."
+//             break;
+//         case error.UNKNOWN_ERROR:
+//             x.innerHTML = "An unknown error occurred."
+//             break;
+//     }
+// }
