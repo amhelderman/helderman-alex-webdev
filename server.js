c0671d5879bf9fb28ad4655c87a343544a1a78bd
amhelderman <alexhelderman@gmail.com>
2017-07-27 19:18:04 -0400

    added the necessary require() call to server.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
+require("./assignment/app");
 
 // from piazza thread 641
 port = process.env.PORT || 3000;
