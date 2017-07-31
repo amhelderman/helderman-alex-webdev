7d07a0954710efa386bc498c5bc751a96cadecdf
amhelderman <alexhelderman@gmail.com>
2017-07-31 17:46:41 -0400

    added directive - bad looking

---
+++ public/assignment/directives/wbdvDirectives.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Alex on 7/31/17.
+ */
+
+(function (){
+
+    angular
+        .module("WamApp")
+        .directive("wbdvsortable", function(){
+            return {
+                template : "<h1>YESSSS</h1>"
+            };
+        });
+
+
+
+
+
+})();
