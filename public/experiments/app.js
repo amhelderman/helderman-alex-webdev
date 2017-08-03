2f725d48030548fe78303e6ac9df2e80f39ecbba
amhelderman <alexhelderman@gmail.com>
2017-08-03 15:28:41 -0400

    jobService experiment to access AuthenticJobs API

--- public/experiments/app.js
+++ public/experiments/app.js
@@ -9,12 +9,42 @@
     angular
         .module("ajApp", ["ngRoute"])
         .controller("ajController", ajController)
+        .service("jobService", jobService);
 
 
-    function ajController($location){
+    function jobService($http)
+    {
+        this.getJobTypes = getJobTypes;
+        this.deletePage = deletePage;
+
+        function getJobTypes(){
+            var url = "https://authenticjobs.com/api/?api_key=2f9cf88dada5697e6eda2c4e8855ccc7&method=aj.types.getlist";
+            console.log("getJobTypes");
+            console.log(url);
+            return $http.get(url);
+        }
+        function deletePage(userId, websiteId, pageId){
+            var url =  "/api/user/"+userId+"/website/"+websiteId+"/page/"+pageId;
+            console.log("page service: deleting page using url:");
+            console.log(url);
+            return $http.delete(url);
+        }
+
+    }
+
+    function ajController($location, jobService){
         var model = this;
         model.message="HI";
 
+        function init(){
+
+            jobService.getJobTypes()
+                .then(function(response){
+                    console.log(response);
+                    model.message = response.data;
+                })
+        }
+        init();
     }
 
 
