9c2c4899fc8fd09c226153cdd313072727a1b913
amhelderman <alexhelderman@gmail.com>
2017-08-11 03:04:10 -0400

    Revert "Database login works (barely)"
    This reverts commit 8436a085598315997d2db67758c52f5f4d0a844a.

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,6 +1,6 @@
 // based on code from jannunzi
 
-var app = require("../express");
+var app = require("../../express");
 
 app.post    ("/api/user/:userId/website/:websiteId/page/:pageId/widget/", createWidget);
 app.get     ("/api/user/:userId/website/:websiteId/page/:pageId/widget/", findAllWidgetsForPage);
