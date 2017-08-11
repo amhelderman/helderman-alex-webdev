dbe0acf961954423ef591545574122372c053df0
amhelderman <alexhelderman@gmail.com>
2017-08-11 18:13:40 -0400

    organizing index page

--- public/project/services/user.service.client.js
+++ public/project/services/user.service.client.js
@@ -8,10 +8,55 @@
 
     function userService($window, $http)
     {
+        this.createUser = createUser;
+        this.getUser = getUser;
+        this.updateUser = updateUser;
+        this.deleteUser = deleteUser;
 
         var users= [
-            {username: "po", password: "po", facebookUser: {}}
+            {_id: 1, username: "alice", password: "alice"},
+            {_id: 2, username: "po", password: "po"}
         ];
+
+        function createUser(user){
+            console.log("Project - User Service Client - createUser ");
+            users.push(user);
+        }
+
+        function getUser(userId){
+            console.log("Project - User Service Client - getUser ");
+            for(var u in users){
+                if (users[u]._id === userId){
+                    return users[u];
+                }
+            }
+            return null;
+        }
+
+        function updateUser(userId, user){
+            console.log("Project - User Service Client - updateUser ");
+            for(var u in users){
+                if (users[u]._id === userId){
+                    users[u] = user;
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        function deleteUser(userId){
+            console.log("Project - User Service Client - deleteUser ");
+            for(var u in users){
+                if (users[u]._id === userId){
+                    var index = users.indexOf(users[u]);
+                    if (index > -1) {
+                        array.splice(index, 1);
+                        return true;
+                    }
+                }
+            }
+            return null;
+        }
 
 
     }
