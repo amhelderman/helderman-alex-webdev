88133faae7de55a07e320bc0837ac6bcb7a19d04
amhelderman <alexhelderman@gmail.com>
2017-08-11 03:51:11 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/assignment'));
 app.use(express.static(__dirname + '/project'));
 
-// require("./test/app");
+require("./test/app");
 require("./assignment/app");
 require("./project/app");
 
