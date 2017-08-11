0073c96c0c3de1d6993f8b323c14370fd5332102
amhelderman <alexhelderman@gmail.com>
2017-08-10 22:10:10 -0400

    try again

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.static(__dirname + '/assignment'));
 
 // allow cross-origin
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-app.use(allowCrossDomain);
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//
+//     next();
+// }
+// app.use(allowCrossDomain);
 
 // require("./test/app");
 require("./assignment/app");
