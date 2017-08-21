6a5475d9d4bd788ce3dda83888693ef6f6e42a95
amhelderman <alexhelderman@gmail.com>
2017-08-21 12:30:22 -0400

    flattened google user schema.

--- public/project/user/controllers/account.controller.client.js
+++ public/project/user/controllers/account.controller.client.js
@@ -16,18 +16,18 @@
         model.message = "Welcome, user!";
 
         function init(){
-            console.log(["Account trying to get user using id ", userId]);
-            userService.getUser(userId)
-                .then(function(response){
-                    model.user = response.data;
-                    console.log(["Account controller checking login status", model.user]);
-                    if(model.user) {
-                        console.log(["Account Controller - getUser: ", model.user]);
-                        model.message = "Welcome, " + model.user.username + "!";
-                    }else{
-                        $location.url("/login");
-                    }
-                })
+            // console.log(["Account trying to get user using id ", userId]);
+            // userService.getUser(userId)
+            //     .then(function(response){
+            //         model.user = response.data;
+            //         console.log(["Account controller checking login status", model.user]);
+            //         if(model.user) {
+            //             console.log(["Account Controller - getUser: ", model.user]);
+            //             model.message = "Welcome, " + model.user.username + "!";
+            //         }else{
+            //             $location.url("/login");
+            //         }
+            //     })
         }
         init();
 
